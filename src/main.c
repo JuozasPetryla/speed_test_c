81fe8d43c303b0c7b58f2dfa1a92e90bc9768b21
JuozasPetryla <itsgrtjuozas@gmail.com>
2026-02-24 21:09:52 +0200

    Basic control flow, argument handling

--- src/main.c
+++ src/main.c
@@ -13,9 +13,10 @@
 #define INIT_SERVER_ARRAY_SIZE 8192
 #define SERVER_LIST_FILENAME "data/speedtest_server_list.json"
 
-ServerArray* parse_server_list();
+ServerArray* _parse_server_list();
 
 typedef struct {
+    bool a_flag;
     bool l_flag;
     bool d_flag;
     bool c_flag;
@@ -24,15 +25,21 @@ typedef struct {
     bool C_flag;
 } OptFlags;
 
+// TODO: Control flow for options
 int main(int argc, char *argv[]) 
 {
-    OptFlags opt_flags = { false, false, false, false, false, false };
+    OptFlags opt_flags = { false, false, false, false, false, false, false };
 
     int opt;
-    while((opt = getopt(argc, argv, "hlbC:c:d::u::")) != -1) 
+    char* country;
+    char* city;
+    while((opt = getopt(argc, argv, "ahlbC:c:d::u::")) != -1) 
     { 
         switch(opt) 
         { 
+            case 'a':
+                opt_flags.a_flag = true;
+                break;
             case 'd':
                 opt_flags.d_flag = true;
                 break;
@@ -40,9 +47,11 @@ int main(int argc, char *argv[])
                 opt_flags.u_flag = true;
                 break;
             case 'C':
+                country = optarg;
                 opt_flags.C_flag = true;
                 break;
             case 'c':
+                city = optarg;
                 opt_flags.c_flag = true;
                 break;
             case 'b':
@@ -52,39 +61,64 @@ int main(int argc, char *argv[])
                 opt_flags.l_flag = true;
                 break;
             case 'h':
-                printf("Available options are hlb:d:u:");
+                printf("Available options are hlb:d:u:\n");
                 break;
             case ':': 
-                printf("Option requires an argument");
+                printf("Option requires an argument\n");
                 break; 
             case '?': 
-                printf("Unrecognized option");
+                printf("Unrecognized option\n");
                 break; 
         } 
     }
 
-    
-    ServerArray *servers = parse_server_list();
-
     CURL *handle = curl_easy_init();
-    Location *location = find_location(handle);
-    printf(
-        "User location: City - %s; Country - %s; Internet service provider - %s\n", 
-        location->city, 
-        location->country, 
-        location->provider
-    );
-    
-    Server* best_server = best_server_by_location(handle, servers, location);
-    printf("Best server for %s -> host %s; id %d\n", location->city, best_server->host, best_server->id);
 
+    ServerArray *servers = _parse_server_list();
+
+    if (opt_flags.l_flag) {
+        Location *location = find_location(handle);
+        printf(
+            "User location: City - %s; Country - %s; Internet service provider - %s\n", 
+            location->city, 
+            location->country, 
+            location->provider
+        );
+    }
+
+    // TODO: handle server selection by location, default to current location
+    if (opt_flags.d_flag) {
+        char* host_url = servers->server[1]->host;
+        printf("host_URL\n");
+        double speed = speed_test(handle, DOWNLOAD, host_url);
+        printf("Download speed for host %s - %.2f Mb/s\n", host_url, speed);
+    }
+
+    if (opt_flags.u_flag) {
+        char* host_url = servers->server[1]->host;
+        double speed = speed_test(handle, UPLOAD, host_url);
+        printf("Upload speed for host %s - %.2f Mb/s\n", host_url, speed);
+    }
+
+    if (opt_flags.b_flag) {
+        Location *location;
+        if (opt_flags.c_flag || opt_flags.C_flag) {
+            location = create_location(country, city, NULL);
+        } else {
+            location = find_location(handle);
+        }
+        Server* best_server = best_server_by_location(handle, servers, location);
+        printf("Best server in location %s, %s - %s\n", location->city, location->country, best_server->host);
+    }
 
     destroy_server_array(servers);
 
     exit(0);
 }
 
-ServerArray* parse_server_list()
+
+
+ServerArray* _parse_server_list()
 {
     FILE *fp = _open_file_safe(SERVER_LIST_FILENAME, "r");
 
@@ -130,6 +164,7 @@ ServerArray* parse_server_list()
         );
     }
 
+    return servers;
 }
 
 
